2267605a375dd123d86bd2c8199d33ee2207d460
thedesignfuture <elvis@designfuture.studio>
2025-05-29 11:03:10 +0300

    Update custom.js
    featureImageCaption

--- custom.js
+++ custom.js
@@ -2015,7 +2015,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 elements.tag.textContent = primaryTag;
                 elements.excerpt.textContent = postExcerpt;
                 elements.title.textContent = postTitle;
-                elements.feature.innerHTML(featureImageCaption);
+                elements.feature.innerHTML = featureImageCaption;
 
                 const metaTags = {
                     title: (document.title = post.title),
